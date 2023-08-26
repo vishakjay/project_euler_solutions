841914acf27d47c66e461abffd1ef2383621ee49
vishakjay <107393021+vishakjay@users.noreply.github.com>
2023-08-26 23:15:39 +0530

    Update problem_1.cpp

--- problem_1.cpp
+++ problem_1.cpp
@@ -1,1 +1,16 @@
+#include<bits/stdc++.h>
 
+using namespace std;
+
+int main(){
+
+long long sum=0;
+  for(int i=0;i<1000;i++){
+    if(i%3==0 || i%5==0){
+       sum+=i;
+    }
+  }
+  cout<<sum<<" ";
+return 0;
+}
+//Amswer is 233168
